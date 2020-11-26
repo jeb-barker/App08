260dbc86e02935e4279c883c48df92dfb65716a1
jeebtheweeb <48389379+jeebtheweeb@users.noreply.github.com>
2020-11-25 22:14:12 -0500

    lock orientation

--- app/src/main/java/com/barkerjeb/app08/MainActivity.java
+++ app/src/main/java/com/barkerjeb/app08/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         dv = findViewById(R.id.drawView);
         Drawable im = ContextCompat.getDrawable(getBaseContext(), R.drawable.background);
         im.setAlpha(115);
